75ad66a7777eae033a715e7d2d7e1b0adcab5310
ken22436653 <ken22436653@gmail.com>
2017-02-09 00:38:15 -0500

    first commit

---
+++ web/models.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+
+class Message(models.Model):
+  user = models.CharField(max_length=50)
+  subject = models.CharField(max_length=200)
+  publication_date = models.DateTimeField()
+  def __unicode__(self):
+     return self.subject
+  
+class Month(models.Model):
+  date = models.IntegerField(default=0)
+  def __unicode__(self):
+    return str(self.date)
+
+class Menu(models.Model):
+  photo = models.ImageField()
+  food = models.CharField(max_length=20)
+  price = models.IntegerField(default=0)
+  def __unicode__(self):
+    return self.food
